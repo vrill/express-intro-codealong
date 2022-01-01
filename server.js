1ed839d9a98b9d301f381379880dce8064e112ef
vrill <52007698+vrill@users.noreply.github.com>
2022-01-01 19:06:10 +0100

    Created a couple of endpoints and a query 'filter'. Codealong finished.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+import data from './data.json'
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -16,6 +18,26 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/nominations', (req, res) => {
+  res.json(data)
+})
+
+app.get('/year/:yearnum', (req, res) => {
+  const yearnum = req.params.yearnum
+  const showWin = req.query.win
+  let nominationsFromYear = data.filter(
+    (item) => item.year_award === +yearnum
+    )
+
+  if (showWin) {
+    nominationsFromYear = nominationsFromYear.filter(
+      (item) => item.win
+      )
+  }
+
+  res.json(nominationsFromYear)
 })
 
 // Start the server
